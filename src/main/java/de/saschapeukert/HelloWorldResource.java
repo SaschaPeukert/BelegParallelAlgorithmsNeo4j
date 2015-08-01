a4d5ae37b7d26e5a31ac3979fced09f1ba5d7023
Sascha Peukert <sascha.peukert@gmail.com>
2015-08-01 17:17:45 +0200

    Added an working HelloWorld UnmanagedExtension.

---
+++ src/main/java/de/saschapeukert/HelloWorldResource.java
@@ -0,0 +1,35 @@
+package de.saschapeukert;
+
+import java.nio.charset.Charset;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+
+//START SNIPPET: HelloWorldResource
+@Path( "/helloworld" )
+public class HelloWorldResource
+{
+    private final GraphDatabaseService database;
+
+    public HelloWorldResource( @Context GraphDatabaseService database )
+    {
+        this.database = database;
+    }
+
+    @GET
+    @Produces( MediaType.TEXT_PLAIN )
+    @Path( "/{nodeId}" )
+    public Response hello( @PathParam( "nodeId" ) long nodeId )
+    {
+        // Do stuff with the database
+        return Response.status( Status.OK ).entity(
+                ("Hello World, nodeId=" + nodeId).getBytes( Charset.forName("UTF-8") ) ).build();
+    }
+}
